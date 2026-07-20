b7a9c1cdaf5a13774af4eb013ab38686ac72fd1f
omshukla0721p <ks0134067@gmail.com>
2026-07-20 22:26:23 +0530

    standard method

--- dp/minimum_cost_to_cut_stick.py
+++ dp/minimum_cost_to_cut_stick.py
@@ -63,3 +63,46 @@ def cost_tabu(n,cuts):
 print(cost_recur(n,cuts))
 print(cost_memo(n,cuts))
 print(cost_tabu(n,cuts))
+
+
+# pattern recognition
+# in this we are looping only on the cuts
+# 
+def min_cost_recur(n,cuts):
+    cuts.sort()
+    cuts=[0]+cuts+[n]
+    def solve(i,j):
+        if j-i==1:
+            return 0
+        cost=float("inf")
+        for k in range(i+1,j):
+            cost=min(cost,solve(i,k)+solve(k,j)+cuts[j]-cuts[i])
+        return cost
+    return solve(0,len(cuts)-1)
+
+print(min_cost_recur(n,cuts))
+
+
+# memoization
+
+def min_cost_memo(n,cuts):
+    cuts.sort()
+    cuts=[0]+cuts+[n]
+
+    dp=[[None for _ in range(len(cuts))] for __ in range(len(cuts))]
+
+    def solve(i,j):
+        if j-i==1:
+            return 0
+        if dp[i][j]!=None:
+            return dp[i][j]
+        cost=float("inf")
+        for k in range(i+1,j):
+            cost=min(cost,solve(i,k)+solve(k,j)+cuts[j]-cuts[i])
+        dp[i][j]=cost
+        return dp[i][j]
+    return solve(0,len(cuts)-1)
+
+print(min_cost_memo(n,cuts))
+
+    
